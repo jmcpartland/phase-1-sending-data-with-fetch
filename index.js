79d05e61d98f604fd223dce036a43bbb5e076f8c
Joe McPartland <joemcpartland@gmail.com>
2021-11-12 17:04:16 -0700

    commit tree 33341d977c0eadc0fa6668e69a9bf5f0eb133290

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+function submitData(name, email) {
+    // const body = document.querySelector("body")
+    // const div = body.createElement("div")
+    // div.innerText = object["id"]
+    
+    let myUser = {
+      name,
+      email
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(myUser),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        document.body.innerHTML = object["id"];
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error;
+    })
+
+
+};
